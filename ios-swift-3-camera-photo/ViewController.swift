8d52c16bc0c3b49031aaf0f845b42e4331696657
ZhaonanLi <zhaonanproject@gmail.com>
2016-11-07 21:21:58 -0800

    Initial Commit

---
+++ ios-swift-3-camera-photo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-swift-3-camera-photo
+//
+//  Created by Zhaonan Li on 11/7/16.
+//  Copyright © 2016 Zhaonan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
